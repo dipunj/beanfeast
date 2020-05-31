674d5ec66cbfb4cd487db16083586f510e174d23
Dipunj Gupta <dipunj.mnnit@gmail.com>
2020-06-01 01:40:59 +0530

    added basic mui theme file

---
+++ frontend/theme/index.tsx
@@ -0,0 +1,20 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const BeanfeastTheme = createMuiTheme({
+	palette: {
+		primary: {
+			main: '#1B98E0',
+		},
+		secondary: {
+			main: '#575860',
+		},
+		error: {
+			main: '#D72638',
+		},
+		info: {
+			main: '#000000',
+		},
+	},
+});
+
+export default BeanfeastTheme;
