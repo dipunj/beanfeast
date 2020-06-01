8558154b0d19da10a33a7d06dbd501a673b20ac8
Dipunj Gupta <dipunj.mnnit@gmail.com>
2020-06-01 07:24:56 +0530

    added fingerprint utility

---
+++ frontend/utils/fingerprint.ts
@@ -0,0 +1,17 @@
+import * as Fingerprint2 from 'fingerprintjs2';
+
+const options = {
+	excludes: {
+		audio: true,
+		enumerateDevices: true,
+	},
+};
+
+const getFingerprint = async () => {
+	const fingPrints = await Fingerprint2.getPromise(options);
+	const values = fingPrints.map((fp) => fp.value);
+	const uniqueGibberish = Fingerprint2.x64hash128(values.join(''), 31);
+	return uniqueGibberish;
+};
+
+export default getFingerprint;
