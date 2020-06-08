335a1c895ac606d2c4f121c18b080b6f1cd04603
Dipunj Gupta <dipunj.mnnit@gmail.com>
2020-06-08 21:10:13 +0530

    initialising dark mode based on global user pref

--- frontend/components/Context/index.tsx
+++ frontend/components/Context/index.tsx
@@ -1,17 +1,19 @@
 import { createContext, useState, useEffect } from 'react';
 import { getFromLS, saveToLS } from '../../utils/localStorage';
+import { useMediaQuery } from '@material-ui/core';
 
 const SessionCtx = createContext({ ctx: { darkMode: false }, setCtx: (old) => {} });
 
 const SessionCtxProvider = ({ children }) => {
 	const [ctx, setCtx] = useState({});
+	const prefersDark = useMediaQuery('(prefers-color-scheme: dark)');
 
 	useEffect(() => {
 		let savedPref = getFromLS('darkMode');
 
 		if (savedPref === null) {
-			// first time visit, create darkMode in localstorage
-			saveToLS('darkMode', false);
+			// first time visit, create darkMode in localstorage using media query prefers-color-scheme
+			saveToLS('darkMode', prefersDark);
 			savedPref = false;
 		}
 
