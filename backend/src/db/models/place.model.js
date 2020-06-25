25077e4910c704fb78cce2b7e2b22e012d4273cd
Dipunj Gupta <dipunj.mnnit@gmail.com>
2020-06-25 16:56:33 +0530

    removed redundant schema files

--- backend/src/db/models/place.model.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-var PlaceSchema = require('../schemas/place.schema');
-
-const { model } = mongoose;
-const Place = model('Place', PlaceSchema);
-
-module.exports = Place;
