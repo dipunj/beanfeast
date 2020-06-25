f03634166d470822d58ab38b5cc7a0266ab201a1
Dipunj Gupta <dipunj.mnnit@gmail.com>
2020-06-26 01:40:39 +0530

    fixed style for detail view

--- frontend/components/Places/CardsAndMap/styles.tsx
+++ frontend/components/Places/CardsAndMap/styles.tsx
@@ -8,7 +8,7 @@ const MapContainer = styled.div`
 	/* border: 1px solid lightgray; */
 	& .leaflet-container {
 		border-radius: 16px;
-		border: 1px solid grey;
+		border: 1px solid lightgray;
 		height: 100%;
 	}
 `;
@@ -17,14 +17,13 @@ const DetailsContainer = styled.div`
 	overflow-y: auto;
 	/* height: 75vh; */
 	/* width: 25%; */
-	height: 100%;
+	height: calc(85vh - 100px);
 	width: 20%;
 	position: fixed;
-	right: 0px;
+	right: 8px;
 	top: 15vh;
-	padding-bottom: 200px;
 	border-radius: 10px;
-	border: 1px solid grey;
+	border: 1px solid lightgray;
 	margin-top: 16px;
 `;
 
