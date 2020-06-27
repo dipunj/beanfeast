ecd03671de62fb7e9ba44b932c106488df3469ed
Dipunj Gupta <dipunj.mnnit@gmail.com>
2020-06-27 15:09:35 +0530

    integrated cogo-toast with new pool page

--- frontend/components/Pool/new/index.tsx
+++ frontend/components/Pool/new/index.tsx
@@ -2,11 +2,11 @@ import { useEffect, useReducer, useState } from 'react';
 import { Grid, Typography, Button, LinearProgress, Paper } from '@material-ui/core';
 import { useRouter } from 'next/router';
 import useStyles from './styles';
-import { NotificationToast, Request, TimePicker, DatePicker } from '../../util';
+import { Request, TimePicker, DatePicker } from '../../util';
 import getBrowserFingerprint from '../../../utils/fingerprint';
 import mergeDateTime from '../../../utils/mergeDateTime';
 import HeadCount, { HeadCountLabel } from './HeadCount';
-import { handleNotification } from '../../util/NotificationToast';
+import Toast from '../../util/Toast';
 
 const initialState = {
 	date: new Date(),
@@ -16,7 +16,6 @@ const initialState = {
 	location: null,
 	apiUnderProgress: false,
 	loadingLocation: true,
-	permissionError: null,
 };
 
 const reducer = (state, action) => {
@@ -34,35 +33,39 @@ const reducer = (state, action) => {
 		case 'setApiUnderProgress':
 			return { ...state, apiUnderProgress: action.apiUnderProgress };
 		case 'setPermissionError':
-			let title = '';
-			let message = '';
 			if (action.hasOwnProperty('error')) {
 				switch (action.error.code) {
 					case action.error.PERMISSION_DENIED:
-						title = 'Permission Error';
-						message = 'You denied the request for Geolocation, cannot proceed.';
+						Toast({
+							title: 'Permission Error',
+							message: 'You denied the request for Geolocation, cannot proceed.',
+							type: 'error',
+						});
 						break;
 					case action.error.POSITION_UNAVAILABLE:
-						title = 'Position Unavailable';
-						message = 'Location information is unavailable on this device';
+						Toast({
+							title: 'Position Unavailable',
+							message: 'Location information is unavailable on this device',
+							type: 'error',
+						});
 						break;
 					case action.error.TIMEOUT:
-						title = 'Timeout';
-						message = 'The request to get user location timed out.';
+						Toast({
+							title: 'Timeout',
+							message: 'The request to get user location timed out.',
+							type: 'error',
+						});
 						break;
 					default:
-						title = 'Unknown Error';
-						message = 'An unknown error occurred.';
+						Toast({
+							title: 'Unknown Error',
+							message: 'An unknown error occurred.',
+							type: 'error',
+						});
 						break;
 				}
-				return {
-					...state,
-					permissionError: { title, message },
-					loadingLocation: false,
-				};
-			} else {
-				return { ...state, permissionError: null, loadingLocation: false };
 			}
+			return { ...state, loadingLocation: false };
 		default:
 			throw new Error('Unexpected action');
 	}
@@ -72,12 +75,6 @@ const createNewPool = () => {
 	const styles = useStyles();
 	const router = useRouter();
 	const [state, dispatch] = useReducer(reducer, initialState);
-	const [notification, setNotification] = useState(null);
-
-	const handleClose = (e: any, reason?: string) => {
-		if (reason === 'clickaway') return;
-		dispatch({ type: 'setPermissionError' });
-	};
 
 	const handleSubmit = async () => {
 		dispatch({ type: 'setApiUnderProgress', apiUnderProgress: true });
@@ -105,7 +102,8 @@ const createNewPool = () => {
 			dispatch({ type: 'setApiUnderProgress', apiUnderProgress: false });
 			router.push(`/pool/status/[poolId]`, `/pool/status/${poolData._id}`);
 		} catch (error) {
-			handleNotification(setNotification, error);
+			Toast({ error });
+			dispatch({ type: 'setApiUnderProgress', apiUnderProgress: false });
 		}
 	};
 
@@ -131,85 +129,73 @@ const createNewPool = () => {
 	}, []);
 
 	return (
-		<>
-			<Grid container direction="row" justify="space-evenly" alignItems="stretch" spacing={4}>
-				<Grid item xs={12}>
-					<DatePicker
-						{...{
-							selectedDate: state.date,
-							setSelectedDate: (date: Date) => dispatch({ type: 'setDate', date }),
-							label: 'Date',
-							minDate: new Date(),
-						}}
-					/>
-				</Grid>
-				<Grid item xs={12} sm={6}>
-					<TimePicker
-						{...{
-							selectedDate: state.fromTime,
-							setSelectedDate: (date: Date) =>
-								dispatch({ type: 'setFromTime', date }),
-							label: 'From',
-						}}
-					/>
-				</Grid>
-				<Grid item xs={12} sm={6}>
-					<TimePicker
-						{...{
-							selectedDate: state.toTime,
-							setSelectedDate: (date: Date) => dispatch({ type: 'setToTime', date }),
-							label: 'To',
-						}}
-					/>
-				</Grid>
-				<Grid item xs={12} container alignItems="center" spacing={4}>
-					<HeadCountLabel />
-					<HeadCount
-						{...{
-							value: state.headCount,
-							sliderOnChange: (_: any, headCount: any) =>
-								dispatch({ type: 'setHeadCount', headCount }),
-							numberOnChange: ({ target: { value: headCount } }) =>
-								dispatch({ type: 'setHeadCount', headCount }),
-						}}
-					/>
-				</Grid>
-				<Grid item xs={12} sm={8} md={6}>
-					<Button
-						{...{
-							color: 'primary',
-							variant: 'outlined',
-							fullWidth: true,
-							size: 'large',
-							variant: 'contained',
-							onClick: handleSubmit,
-							disabled: state.location === null,
-							className: styles.squareButton,
-						}}
-						// style={{ color: 'white', background: theme.palette.warning.main }}
-					>
-						Create Pool
-					</Button>
-					{(state.loadingLocation || state.apiUnderProgress) && (
-						<Grid item xs={12}>
-							<LinearProgress style={{ width: '100%' }} color="primary" />
-							<Typography color="textSecondary" align="center">
-								{state.loadingLocation && 'Acquiring GPS'}
-							</Typography>
-						</Grid>
-					)}
-				</Grid>
+		<Grid container direction="row" justify="space-evenly" alignItems="stretch" spacing={4}>
+			<Grid item xs={12}>
+				<DatePicker
+					{...{
+						selectedDate: state.date,
+						setSelectedDate: (date: Date) => dispatch({ type: 'setDate', date }),
+						label: 'Date',
+						minDate: new Date(),
+					}}
+				/>
 			</Grid>
-			<NotificationToast
-				{...{
-					isOpen: state.permissionError !== null,
-					handleClose,
-					title: state.permissionError?.title,
-					message: state.permissionError?.message,
-					type: 'error',
-				}}
-			/>
-		</>
+			<Grid item xs={12} sm={6}>
+				<TimePicker
+					{...{
+						selectedDate: state.fromTime,
+						setSelectedDate: (date: Date) => dispatch({ type: 'setFromTime', date }),
+						label: 'From',
+					}}
+				/>
+			</Grid>
+			<Grid item xs={12} sm={6}>
+				<TimePicker
+					{...{
+						selectedDate: state.toTime,
+						setSelectedDate: (date: Date) => dispatch({ type: 'setToTime', date }),
+						label: 'To',
+					}}
+				/>
+			</Grid>
+			<Grid item xs={12} container alignItems="center" spacing={4}>
+				<HeadCountLabel />
+				<HeadCount
+					{...{
+						value: state.headCount,
+						sliderOnChange: (_: any, headCount: any) =>
+							dispatch({ type: 'setHeadCount', headCount }),
+						numberOnChange: ({ target: { value: headCount } }) =>
+							dispatch({ type: 'setHeadCount', headCount }),
+					}}
+				/>
+			</Grid>
+			<Grid item xs={12} sm={8} md={6}>
+				<Button
+					{...{
+						color: 'primary',
+						variant: 'outlined',
+						fullWidth: true,
+						size: 'large',
+						variant: 'contained',
+						onClick: handleSubmit,
+						disabled: state.location === null,
+						className: styles.squareButton,
+					}}
+					// style={{ color: 'white', background: theme.palette.warning.main }}
+				>
+					Create Pool
+				</Button>
+				{(state.loadingLocation || state.apiUnderProgress) && (
+					<Grid item xs={12}>
+						<LinearProgress style={{ width: '100%' }} color="primary" />
+						<Typography color="textSecondary" align="center">
+							{state.loadingLocation && 'Acquiring GPS'}
+						</Typography>
+					</Grid>
+				)}
+			</Grid>
+		</Grid>
 	);
 };
 
