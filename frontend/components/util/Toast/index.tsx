7ca7dc2005f80d9c65b49857024ff220a59fb5cf
Dipunj Gupta <dipunj.mnnit@gmail.com>
2020-06-27 16:50:50 +0530

    added styles to toast container, so that content always fits in viewport removed old notification toast component

--- frontend/components/util/Toast/index.tsx
+++ frontend/components/util/Toast/index.tsx
@@ -1,19 +1,19 @@
 import toast from 'cogo-toast';
-import { TitleDiv, MessageDiv } from './styles';
+import { TitleDiv, MessageDiv, Container } from './styles';
 
 const Toast = ({ type = 'info', title, message, error = null }) =>
 	error
 		? toast.error(
-				<>
+				<Container>
 					{<TitleDiv>{error.response.data.meta.response.title}</TitleDiv>}
 					{<MessageDiv>{error.response.data.meta.response.message}</MessageDiv>}
-				</>
+				</Container>
 		  )
 		: toast[type](
-				<>
+				<Container>
 					{title && <TitleDiv>{title}</TitleDiv>}
 					{message && <MessageDiv>{message}</MessageDiv>}
-				</>
+				</Container>
 		  );
 
 export default Toast;
--- frontend/components/util/Toast/styles.tsx
+++ frontend/components/util/Toast/styles.tsx
@@ -1,6 +1,16 @@
 import styled from 'styled-components';
 
-const TitleDiv = styled.div``;
-const MessageDiv = styled.div``;
+const TitleDiv = styled.div`
+	width: 100%;
+`;
+const MessageDiv = styled.div`
+	width: 100%;
+`;
+const Container = styled.div`
+	max-width: 60vw;
+	* {
+		overflow-wrap: break-word;
+	}
+`;
 
-export { TitleDiv, MessageDiv };
+export { TitleDiv, MessageDiv, Container };
