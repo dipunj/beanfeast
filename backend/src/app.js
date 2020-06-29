db1a612bcfec13712cb80fe3222ed7c53d99d633
Dipunj Gupta <dipunj.mnnit@gmail.com>
2020-06-29 22:58:52 +0530

    added frontend app url env for backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ var poolRouter = require('./routes/pool.routes');
 var placeRouter = require('./routes/place.routes');
 
 const app = express();
-app.use(cors({ credentials: true, origin: ['http://localhost:3000'] }));
+app.use(cors({ credentials: true, origin: [process.env.FRONTEND_APP_URL] }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
