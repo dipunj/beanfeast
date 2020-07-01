1529e4cabd2ddf568b5cd6d3d06af95e5f9aa735
Dipunj Gupta <dipunj.mnnit@gmail.com>
2020-07-01 15:23:23 +0530

    fixed auto remove via index

--- backend/src/db/schemas/pool.schema.js
+++ backend/src/db/schemas/pool.schema.js
@@ -4,46 +4,42 @@ var { Decimal128 } = require('mongodb');
 
 const { Schema } = mongoose;
 
-const PoolSchema = new Schema(
-	{
-		_id: {
-			type: String,
-			default: shortid.generate,
-		},
-		fromTime: {
-			type: Date,
-			default: Date.now(),
-		},
-		toTime: {
-			type: Date,
-			default: Date.now(),
-		},
-		maxPoolSize: {
-			type: Number,
-			default: Infinity,
-		},
-		currPoolSize: {
-			type: Number,
-			default: 0,
-		},
-		createdBy: {
-			type: String,
-		},
-		centroidLatitude: {
-			type: Decimal128,
-		},
-		centroidLongitude: {
-			type: Decimal128,
-		},
-		createdAt: {
-			type: Date,
-			expires: 86400,
-			default: Date.now,
-		},
+const PoolSchema = new Schema({
+	_id: {
+		type: String,
+		default: shortid.generate,
 	},
-	{
-		timestamps: true,
-	}
-);
+	fromTime: {
+		type: Date,
+		default: Date.now(),
+	},
+	toTime: {
+		type: Date,
+		default: Date.now(),
+	},
+	maxPoolSize: {
+		type: Number,
+		default: Infinity,
+	},
+	currPoolSize: {
+		type: Number,
+		default: 0,
+	},
+	createdBy: {
+		type: String,
+	},
+	centroidLatitude: {
+		type: Decimal128,
+	},
+	centroidLongitude: {
+		type: Decimal128,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+PoolSchema.index({ createdAt: 1 }, { expireAfterSeconds: 86400 });
 
 module.exports = PoolSchema;
--- backend/src/db/schemas/session.schema.js
+++ backend/src/db/schemas/session.schema.js
@@ -3,34 +3,30 @@ var { Decimal128 } = require('mongodb');
 
 const { Schema } = mongoose;
 
-const SessionSchema = new Schema(
-	{
-		poolId: {
-			type: String,
-			ref: 'Pool',
-			index: true,
-		},
-		uniqueIdentifier: {
-			type: String,
-			required: true,
-		},
-		longitude: {
-			type: Decimal128,
-			required: true,
-		},
-		latitude: {
-			type: Decimal128,
-			required: true,
-		},
-		createdAt: {
-			type: Date,
-			expires: 86400,
-			default: Date.now,
-		},
+const SessionSchema = new Schema({
+	poolId: {
+		type: String,
+		ref: 'Pool',
+		index: true,
 	},
-	{
-		timestamps: true,
-	}
-);
+	uniqueIdentifier: {
+		type: String,
+		required: true,
+	},
+	longitude: {
+		type: Decimal128,
+		required: true,
+	},
+	latitude: {
+		type: Decimal128,
+		required: true,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+SessionSchema.index({ createdAt: 1 }, { expireAfterSeconds: 86400 });
 
 module.exports = SessionSchema;
