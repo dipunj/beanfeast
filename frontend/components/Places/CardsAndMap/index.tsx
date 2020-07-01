82ff0e612d30b9b957c362ef925709667ddd8e47
Dipunj Gupta <dipunj.mnnit@gmail.com>
2020-07-02 01:58:08 +0530

    loading leaflet css in the component itself using next/head, and leaflet js using script tag

--- frontend/components/Places/CardsAndMap/index.tsx
+++ frontend/components/Places/CardsAndMap/index.tsx
@@ -5,6 +5,7 @@ import PlacesCard from '../PlaceCard';
 import sortHullOrder from '../../../common/utils/hull';
 import ToolTip from '../ToolTip';
 import { coordinate, resultResponse } from '../../../common/types';
+import Head from 'next/head';
 
 // leaflet doesn't support ssr
 const MapView = dynamic(import('../PlaceMaps'), {
@@ -161,4 +162,27 @@ const CardsAndMap = ({ isMobile, data }: { isMobile: boolean; data: resultRespon
 	);
 };
 
-export default CardsAndMap;
+const withLeafletCSS = (Component) => (props) => {
+	return (
+		<>
+			<Head>
+				{/* leaflet css */}
+				<link
+					rel="stylesheet"
+					href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+					integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+					crossOrigin=""
+				/>
+			</Head>
+			<Component {...props} />
+			{/* leaflet javascript */}
+			<script
+				src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+				integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+				crossOrigin=""
+				async
+			/>
+		</>
+	);
+};
+export default withLeafletCSS(CardsAndMap);
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -42,14 +42,6 @@ export default function MyApp(props) {
 					rel="stylesheet"
 					key="google-font-condiment"
 				/>
-
-				{/* leaflet css */}
-				<link
-					rel="stylesheet"
-					href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
-					integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
-					crossOrigin=""
-				/>
 			</Head>
 			<SessionCtxProvider>
 				<Providers>
@@ -58,13 +50,6 @@ export default function MyApp(props) {
 					{getLayout(<Component {...pageProps} />)}
 				</Providers>
 			</SessionCtxProvider>
-
-			{/* leaflet javascript */}
-			<script
-				src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
-				integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
-				crossOrigin=""
-			/>
 		</>
 	);
 }
